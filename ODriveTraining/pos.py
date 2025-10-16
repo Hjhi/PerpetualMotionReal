6f0a63e4f7d6b37ee0071958eff0836a215489cd
softdev <herkyaaron@gmail.com>
2025-10-16 13:27:49 -0700

    ;alkjdf;lksajdf;lkasjfd

---
+++ ODriveTraining/pos.py
@@ -0,0 +1,20 @@
+from dpea_odrive.odrive_helpers import *
+from time import sleep
+
+od = find_odrive()
+assert od.config.enable_brake_resistor is True, "Check for faulty brake resistor."
+
+ax = ODriveAxis(od.axis0)
+ax.set_gains()
+if not ax.is_calibrated():
+    print("calibrating...")
+    ax.calibrate()
+
+ax.set_vel_limit(5)
+ax.set_pos(5)
+ax.wait_for_motor_to_stop()
+print("Current Position in Turns = ", round(ax.get_pos(), 2))
+ax.set_relative_pos(-5)
+ax.wait_for_motor_to_stop()
+print("Current Position in Turns = ", round(ax.get_pos(), 2))
+sleep(3)
