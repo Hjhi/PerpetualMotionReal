f16df275a7b5e1927e1925e6d95fd6573a4f7d36
softdev <herkyaaron@gmail.com>
2025-10-24 14:58:49 -0700

    push to pi

--- PerpetualMotion/PerpetualMachine.py
+++ PerpetualMotion/PerpetualMachine.py
@@ -83,7 +83,7 @@ class PerpetualMachine:
             sleep(0.02)
         self.dpiStepper.enableMotors(False)
         print("motor startup finished")
-        print(self.dpiStepper.getAllMotorsStopped())
+        print(self.dpiStepper.getStepperStatus(0))
 
     def set_stair_speed(self, speed: float):
         s = speed * 40
