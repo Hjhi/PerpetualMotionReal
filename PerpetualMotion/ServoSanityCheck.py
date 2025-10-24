dbeda4180b9dd617559f5ecdb99dc1542898030c
softdev <herkyaaron@gmail.com>
2025-10-24 14:24:18 -0700

    push to pi

--- PerpetualMotion/ServoSanityCheck.py
+++ PerpetualMotion/ServoSanityCheck.py
@@ -4,13 +4,10 @@ from time import sleep
 def main(m: PerpetualMachine):
     m.turn_stairs_on()
     while True:
-        print("high speed")
-        m.set_stair_speed(100)
-        sleep(5)
-        print("low speed")
-        m.set_stair_speed(30)
-        sleep(5)
-
+        for i in range(100):
+            print(i)
+            m.set_stair_speed(i)
+            sleep(1)
 
 if __name__ == "__main__":
     machine = PerpetualMachine()
