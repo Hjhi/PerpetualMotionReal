c571ddd743ec36e79b18df3a8b7f4155391234fc
softdev <herkyaaron@gmail.com>
2025-10-24 14:41:05 -0700

    push to pi

--- PerpetualMotion/PerpetualMachine.py
+++ PerpetualMotion/PerpetualMachine.py
@@ -86,7 +86,7 @@ class PerpetualMachine:
 
     def set_stair_speed(self, speed: float):
         s = speed * 40
-        self.stair_speed = 0 if speed == 0 else int(90 - 20 + speed)
+        self.stair_speed = 0 if speed == 0 else int(90 - (20 + speed))
         print(self.stair_speed)
         if self.stair_power == self.OnOffState.ON:
             self.turn_stairs_on()
