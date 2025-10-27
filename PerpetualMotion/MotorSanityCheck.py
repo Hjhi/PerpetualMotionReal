0a0ae72c47eae439238fd5d67250d305daf85002
softdev <herkyaaron@gmail.com>
2025-10-27 14:46:41 -0700

    save to pi

--- PerpetualMotion/MotorSanityCheck.py
+++ PerpetualMotion/MotorSanityCheck.py
@@ -1,6 +1,12 @@
 from PerpetualMachine import PerpetualMachine
 
 def main(m: PerpetualMachine):
+    m.startup()
+    m.turn_ramp_on()
+    m.set_ramp_speed(0.5)
+    while True:
+        m.run_ramp_auto()
+
 
 
 if __name__ == "__main__":
