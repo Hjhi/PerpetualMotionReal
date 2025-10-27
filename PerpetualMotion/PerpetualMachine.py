171f4b6bc43e5a05591c1bf3d2a26a27d84cd837
softdev <herkyaaron@gmail.com>
2025-10-27 09:30:23 -0700

    save to pi

--- PerpetualMotion/PerpetualMachine.py
+++ PerpetualMotion/PerpetualMachine.py
@@ -37,7 +37,7 @@ class PerpetualMachine:
     gate_state: GateState = GateState.CLOSED
 
     queue_auto_manual_change: bool = False
-    ramp_top_pos: float = 55 # revolutions
+    ramp_top_pos: float = 27 # revolutions
 
     def __init__(self, **kwargs):
         self.dpiComputer = DPiComputer()
