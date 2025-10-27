6c74be75cb7444c77095dea90ce20d34b1af5a76
softdev <herkyaaron@gmail.com>
2025-10-27 14:54:59 -0700

    save to pi

--- PerpetualMotion/MotorSanityCheck.py
+++ PerpetualMotion/MotorSanityCheck.py
@@ -1,17 +1,37 @@
+from dpeaDPi.DPiStepper import DPiStepper
+from time import sleep
+
 from PerpetualMachine import PerpetualMachine
 
-def main(m: PerpetualMachine):
-    m.startup()
-    m.turn_ramp_on()
-    m.set_ramp_speed(0.5)
-    while True:
-        m.run_ramp_auto()
+def main():
+    dpiStepper = DPiStepper()
+    dpiStepper.setMicrostepping(8)
+    dpiStepper.setSpeedInRevolutionsPerSecond(0, 3)
+    dpiStepper.setAccelerationInRevolutionsPerSecondPerSecond(0, 3)
+    dpiStepper.enableMotors(True)
+
+    #  Exit:   [0]: True returned on success, else False
+    #          [1]: True returned if motor is stopped
+    #          [2]: True returned if motors are enabled
+    #          [3]: True returned if the "Homing" switch indicates "At home"
+    #
+    for _ in range(3):
+        dpiStepper.moveToAbsolutePositionInRevolutions(0, 28, False)
+
+        while not dpiStepper.getStepperStatus(0)[1]:
+            sleep(0.02)
+
+        dpiStepper.moveToHomeInRevolutions(0, 1, 3, 999999)
+
+        while not dpiStepper.getStepperStatus(0)[3]:
+            sleep(0.02)
+
+
 
 
 
 if __name__ == "__main__":
-    machine = PerpetualMachine()
     try:
-        main(machine)
+        main()
     finally:
-        machine.halt()
+        DPiStepper().enableMotors(False)
