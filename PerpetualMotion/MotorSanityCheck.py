12cbccb0a8e9bfb823baef1690c379d22b484f14
softdev <herkyaaron@gmail.com>
2025-10-27 14:20:30 -0700

    save to pi

--- PerpetualMotion/MotorSanityCheck.py
+++ PerpetualMotion/MotorSanityCheck.py
@@ -2,6 +2,7 @@ from PerpetualMachine import PerpetualMachine
 
 def main(m: PerpetualMachine):
     m.startup()
+    m.dpiStepper.enableMotors(True)
     m.set_ramp_speed(0.5)
     while True:
         m.run_ramp_auto()
