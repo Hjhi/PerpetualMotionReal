2f29c4c38111f4e36ba474e7575dd57c8a85c998
softdev <herkyaaron@gmail.com>
2025-10-27 14:25:32 -0700

    save to pi

--- PerpetualMotion/PerpetualMachine.py
+++ PerpetualMotion/PerpetualMachine.py
@@ -68,9 +68,9 @@ class PerpetualMachine:
                     self.open_gate()
                 if status[3] and not self.dpiComputer.readDigitalIn(self.prox_sensor_bottom):
                     print("got to bottom")
-                    self.set_ramp_speed(self.ramp_speed)
                     self.ramp_state = self.RampState.EJECT
                     self.running = False
+                    self.set_ramp_speed(self.ramp_speed)
 
             case self.RampState.EJECT:
 
